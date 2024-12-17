077b8daf2524b15adedced80f30e9590b441fe9c
Patricio Zuniga <patricio.zunigag@hotmail.com>
2024-12-17 18:21:39 +0100

    small fixes FE

---
+++ apps/web/app/utils/trpc.ts
@@ -0,0 +1,13 @@
+import {
+  createTRPCReact,
+  type inferReactQueryProcedureOptions,
+} from "@trpc/react-query";
+import type { AppRouter } from "@server/trpc/trpc.router";
+import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+
+// infer the types for your router
+export type ReactQueryOptions = inferReactQueryProcedureOptions<AppRouter>;
+export type RouterInputs = inferRouterInputs<AppRouter>;
+export type RouterOutputs = inferRouterOutputs<AppRouter>;
+
+export const trpc = createTRPCReact<AppRouter>();
